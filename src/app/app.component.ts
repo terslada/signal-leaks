3fe1af1137309aaa5cc8d00847668b4c0e76959d
terslada <adam.tersl@tietoevry.com>
2023-12-04 13:02:52 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject, Injectable, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GlobalStore {
+  a = signal('a');
+}
+
+@Component({
+  selector: 'app-nested',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  template: `<div>{{store.a()}}</div>`
+})
+export class NestedComponent {
+  store = inject(GlobalStore);
+}
+
+@Component({
+  selector: 'app-routed',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  template: `<div>{{store.a()}}</div>`
+})
+export class RoutedComponent {
+  store = inject(GlobalStore);
+}
+
+@Component({
+  selector: 'app-default',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterLink, NestedComponent],
+  template: `<button routerLink="routed">navigate</button>
+  <app-nested *ngIf="showNested"></app-nested>
+  <button (click)="showNested = !showNested">show nested</button>`
+})
+export class DefaultComponent {
+  showNested = false;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule, RouterOutlet, RoutedComponent],
+  template: `
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
   title = 'signal-leaks';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DefaultComponent, RoutedComponent } from "./app.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'routed',
+    component: RoutedComponent,
+  },
+  {
+    path: '',
+    component: DefaultComponent,
+  }
+];
